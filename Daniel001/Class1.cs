41d703a9bce35e1dcf4b73dcb47861def7437c5d
Harry van Leeuwen <gidonco.net@gmail.com>
2017-11-25 19:30:59 +0100

    Add project files.

---
+++ Daniel001/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daniel001
+{
+    public class Class1
+    {
+    }
+}
